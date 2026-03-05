0c76f4d4e71fba2fd9f56778a6d55867e9ef1230
ABEL PAREDES VASQUEZ <apeva1989@gmail.com>
2026-03-05 21:51:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9577590128-2a430",
+  "appId": "1:923279368458:web:9fc6bb40a42f1a973b5af3",
+  "apiKey": "AIzaSyCff19vV3AKthoNQ-gGmNoQhLO2j-OPgV0",
+  "authDomain": "studio-9577590128-2a430.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "923279368458"
+};
